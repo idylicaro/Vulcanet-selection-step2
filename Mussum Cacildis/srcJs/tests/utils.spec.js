63b0e58bb307ab03cfb21c220632deeb1b3095f4
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-29 12:35:29 -0300

    test: ensure return correct Pn, Pn = (n!)/(n-3)!

--- Mussum Cacildis/srcJs/tests/utils.spec.js
+++ Mussum Cacildis/srcJs/tests/utils.spec.js
@@ -1,8 +1,13 @@
 const sut = require('../utils')
 describe('Utils ', () => {
-    test('Should return vector with 2 index and correct values and sort ', () => {
+    test('Should return vector with 2 index and correct values ', () => {
         const result = sut.convertCaseLine('2 1')
-        expect(result[0]).toBe(1)
-        expect(result[1]).toBe(2)
+        expect(result[0]).toBe(2)
+        expect(result[1]).toBe(1)
+    });
+    
+    test('Should return correct Pn, Pn = (n!)/(n-3)! ', () => {
+        let result = sut.permute(5)
+        expect(result.length).toBe(60)
     });
 });
