4ab4f67aa1e27ca2b3746ba2b52eea06df2f7176
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-28 23:37:51 -0300

    temp: incomplete trying...

--- Mussum Cacildis/srcJs/index.js
+++ Mussum Cacildis/srcJs/index.js
@@ -3,10 +3,22 @@ const utils = require('./utils')
 
 
 const nCases = readlineSync.question()
-for(let x=0; x < nCases; x++){
+for (let x = 0; x < nCases; x++) {
     const nCans = readlineSync.question()
     const cans = utils.convertCaseLine(readlineSync.question())
-}
-console.log(nCases)
 
-// p i , p j , p k tal que i <j <k e p i <p j e p j > p k .
+    let permuts = new Array()
+
+    for (let x = 0; x < nCans; x++)
+        for (let j = 0; j < nCans; j++)
+            for (let g = 0; g < nCans; g++)
+                if (x != j && x != g && j != g)
+                    permuts.push([x, j, g])
+                    
+    let flag = false;
+    flag = utils.verifyValidPermut(cans, permuts)
+
+    if (!flag) {
+        console.log('NO')
+    }
+}
--- Mussum Cacildis/srcJs/utils.js
+++ Mussum Cacildis/srcJs/utils.js
@@ -3,12 +3,47 @@ const convertCaseLine = (str) => {
     for (let index = 0; index < aux.length; index++) {
         aux[index] = parseInt(aux[index])
     }
-    return aux.sort((a, b) => a - b)
+    return aux
 }
-const permutation = (resultTotal,vector, vectorSize) => {
-    
+
+const permute = (permutation) => {
+    var length = permutation.length,
+        result = [permutation.slice()],
+        c = new Array(length).fill(0),
+        i = 1, k, p;
+
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i]; // i % 2 != 0 , so k = c[i]
+            p = permutation[i];
+            permutation[i] = permutation[k];
+            permutation[k] = p;
+            ++c[i];
+            i = 1;
+            result.push(permutation.slice());
+        } else {
+            c[i] = 0;
+            ++i;
+        }
+    }
+    return result;
+}
+
+const verifyValidPermut = (cans, permuts) => {
+    for (let x = 0; x < permuts.length; x++) {
+        let i = permuts[x][0], j = permuts[x][1], k = permuts[x][2];
+
+        if ((i < j < k) && (cans[i] < cans[j] && cans[j] > cans[k])) {
+            console.log('YES')
+            console.log(i + 1, j + 1, k + 1)
+            return true;
+        }
+    }
+    return false
 }
 
 module.exports = {
-    convertCaseLine
+    convertCaseLine,
+    permute,
+    verifyValidPermut
 }
